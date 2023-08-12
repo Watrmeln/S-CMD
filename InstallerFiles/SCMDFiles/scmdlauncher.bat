4c1775fd3fa9fb8ce54a844ebbc7b9306bd41122
disasterocks <82711453+disasterocks@users.noreply.github.com>
2023-08-12 19:41:30 +0100

    Update scmdlauncher.bat

--- InstallerFiles/SCMDFiles/scmdlauncher.bat
+++ InstallerFiles/SCMDFiles/scmdlauncher.bat
@@ -19,3 +19,11 @@ echo             .:-----:.
 echo -------------------------------------------•
 echo S-CMD LAUNCHER         BUILD 1 - AUG 12 2023
 echo -------------------------------------------•
+echo.
+echo Welcome to the S-CMD Launcher! What would you like to do?
+echo Pick the number corresponding to your choice and press ENTER.
+echo -------------------------------------------•
+echo 1. Launch explorer (default windows shell)
+echo 2. Launch into CMD (Command Prompt)
+echo 3. Launch into CMD as administrator
+echo 4. Power options
