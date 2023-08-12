31b9c5cd5a164d0943f5912045fff71155664ac8
Watrmeln <79606063+Watrmeln@users.noreply.github.com>
2023-08-12 16:09:17 -0400

    Create scmdinfo.bat

---
+++ InstallerFiles/SCMDFiles/scmdinfo.bat
@@ -0,0 +1,30 @@
+@echo off
+echo           :-=+++++++=-:           
+echo        :=+++++++++++++++=:        
+echo      .+++++++-:::::-+++++++.      
+echo     :++++++-         -++++++-     
+echo    .+++++++-          +++++++:    
+echo    ++++++++++-:..     ++++++++    
+echo    +++++++++++++++++=-++++++++.   
+echo    ++++++++ ..:--=++++++++++++    
+echo    -+++++++         .=+++++++=    
+echo     +++++++:         .+++++++     
+echo      =++++++-.      .=+++++=      
+echo       :+++++++++==++++++++:       
+echo         .-+++++++++++++-.         
+echo             .:-----:.
+echo -------------------------------------------0
+echo S-CMD LAUNCHER         BUILD 1 - AUG 12 2023
+echo -------------------------------------------0
+echo.
+echo Startup-CMD by the Melnheads (MHWorks)
+echo -------------------------------------------0
+echo.
+echo WATRMELN (Justin)
+echo Designer, Text Editor, Logo Maker
+echo.
+echo DISASTEROCKS (Lukas)
+echo Scripter, Text Editor, Original Idea
+echo -------------------------------------------0
+echo.
+echo THANK YOU FOR USING S-CMD!
