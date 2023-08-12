cf300912cb4a0512b8bb0da70787fd4c8f1195ee
disasterocks <82711453+disasterocks@users.noreply.github.com>
2023-08-12 21:20:16 +0100

    Update scmdhelp.bat

---
+++ InstallerFiles/SCMDFiles/scmdhelp.bat
@@ -0,0 +1,9 @@
+@echo off
+echo.
+echo SCMDINFO	Shows info about the current build of S-CMD, alongside credits
+echo SCMDPOWER	Gives you the option to power off, restart or return to the launcher.
+echo SCMDLAUNCHER	Returns to the SCMD Launcher
+echo USERTOOL	Tool that allows you to create and delete users
+echo USER	Prints the current user's path
+echo TIME	Displays the current date and time
+@echo on
