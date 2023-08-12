fee6e88f6193384e6e7a2ec7f4b2e9f78223d5ae
Watrmeln <79606063+Watrmeln@users.noreply.github.com>
2023-08-12 16:22:25 -0400

    k

--- InstallerFiles/SCMDFiles/scmdhelp.bat
+++ InstallerFiles/SCMDFiles/scmdhelp.bat
@@ -1,7 +1,7 @@
 @echo off
 echo.
 echo SCMDINFO	Shows info about the current build of S-CMD, alongside credits
-echo SCMDPOWER	Gives you the option to power off, restart or return to the launcher.
+echo PWOPTIONS	Gives you the option to power off, restart or return to the launcher.
 echo SCMDLAUNCHER	Returns to the SCMD Launcher
 echo USERTOOL	Tool that allows you to create and delete users
 echo USER	Prints the current user's path
