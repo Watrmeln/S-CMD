ab4ebbfdcdb11481fd5d1ad598e71e47590cf12a
Watrmeln <79606063+Watrmeln@users.noreply.github.com>
2023-08-12 16:59:51 -0400

    Update scmdhelp.bat

--- InstallerFiles/SCMDFiles/scmdhelp.bat
+++ InstallerFiles/SCMDFiles/scmdhelp.bat
@@ -1,9 +1,12 @@
 @echo off
 echo.
+echo S-CMD Commands
 echo SCMDINFO	Shows info about the current build of S-CMD, alongside credits
 echo SCMDPOWER Gives you the option to power off or restart
 echo SCMDLAUNCHER	Returns to the SCMD Launcher
 echo.
+echo Windows Commands
+echo msedge	Opens Microsoft Edge
 echo sndvol	Opens volume mixer
 echo control	Opens control panel
 echo calc	Opens the calculator app
@@ -11,6 +14,7 @@ echo mspaint	Opens microsoft paint
 echo notepad	Opens microsoft notepad
 echo explorer	Opens up the default windows shell (explorer.exe)
 echo.
+echo Tool Pack Commands*
 echo USERTOOL*	Tool that allows you to create and delete users
 echo USER*	Prints the current user's path
 echo TIME*	Displays the current date and time
